f981a04dd34b0550247e7fe05417a7d2012e251a
MarcoFonti <theemporiumg@gmail.com>
2024-03-22 17:08:23 +0100

    add migrations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,5 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 }
